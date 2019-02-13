dbe4ac0560af3b4835ced8c6fe85089184466233
120356aa <yurlovandrew@gmail.com>
2019-02-12 20:45:48 -0800

    set up styled components

--- luncher/src/App.js
+++ luncher/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from './styles';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        
       </div>
     );
   }
---
+++ luncher/src/styles/appStyles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  border: 1px solid blue;
+`
