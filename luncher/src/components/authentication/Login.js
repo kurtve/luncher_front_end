d6bcda36e035927dc9103518baf09998387af246
120356aa <yurlovandrew@gmail.com>
2019-02-13 21:03:08 -0800

    minor

--- luncher/src/components/authentication/Login.js
+++ luncher/src/components/authentication/Login.js
@@ -61,7 +61,7 @@ class Login extends Component {
                       value="user"
                       required
                     />
-                    <RadioLable htmlFor="admin">User</RadioLable>
+                    <RadioLable htmlFor="admin">Doner</RadioLable>
                   </div>
                   <div>
                     <InputRadio 
