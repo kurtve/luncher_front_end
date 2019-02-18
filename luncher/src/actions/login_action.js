635429f78eefeefd9c0ee6c4d8d832ee153b9e46
120356aa <yurlovandrew@gmail.com>
2019-02-17 16:12:03 -0800

    created registeruser action

--- luncher/src/actions/login_action.js
+++ luncher/src/actions/login_action.js
@@ -11,3 +11,25 @@ export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const GET_USERINFO_START = 'GET_USERINFO_START';
 export const GET_USERINFO_SUCCESS = 'GET_USERINFO_SUCCESS';
 export const GET_USERINFO_FAILURE = 'GET_USERINFO_FAILURE';
+
+export const registerUser = user => dispatch => {
+	dispatch({ type: REGISTER_START });
+	axios({
+		method: 'post',
+		url: ``,
+		data: {
+			email: user.email,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			password: user.password,
+			userRole: user.userRole,
+			userName: user.userName,
+		},
+	})
+		.then(res => {
+			dispatch({ type: REGISTER_SUCCESS, payload: res });
+			localStorage.setItem('id', res.data[0]);
+			window.location.reload();
+		})
+		.catch(err => dispatch({ type: REGISTER_FAILURE, payload: err }));
+};
